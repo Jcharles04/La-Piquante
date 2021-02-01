126bebee7c4a14aac258656de338704a5b7d4515
Jcharles04 <j.charles031290@gmail.com>
2021-02-01 15:02:02 +0100

    commit

---
+++ backend/controllers/sauces.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const Sauces = require('../models/Sauces');
+const fs = require('fs');
+
+exports.createSauces = (req, res, next) => {
+    const saucesObject = JSON.parse(req.body.thing);
+    delete thingObject._id;
+    const sauces = new Sauces({
+        ...saucesObject,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    });
+    Sauces.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.modifySauces = (req, res, next) => {
+    const saucesObject = req.file ?
+        {
+        ...JSON.parse(req.body.thing),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        } : { ...req.body };
+    Sauces.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.deleteSauces = (req, res, next) => {
+    Sauces.findOne({ _id: req.params.id })
+        .then(sauces => {
+            const filename = sauces.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`, () => {
+                Sauces.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+                    .catch(error => res.status(400).json({ error }));
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
+};
+
+exports.getOneSauces = (req, res, next) => {
+    Sauces.findOne({ _id: req.params.id })
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(404).json({ error }));
+};
+
+exports.getAllSauces = (req, res, next) => {
+    Sauces.find()
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }));
+};
+
+
+
---
+++ backend/models/Sauces.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+const saucesSchema = mongoose.Schema({
+    userId: { type: String, require: true },
+    name: { type: String, require: true },
+    manufacturer: { type: String, require: true },
+    description: { type: String, require: true },
+    mainPepper: { type: String, require: true },
+    imageUrl: { type: String, require: true },
+    heat: { type: Number, require: true },
+});
+
+module.exports = mongoose.model("Sauces", saucesSchema);
