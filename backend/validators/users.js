f40de26f02f4745d35094212f591aa0185b799e1
Jcharles04 <j.charles031290@gmail.com>
2021-02-05 15:58:30 +0100

    1.0

--- backend/validators/users.js
+++ backend/validators/users.js
@@ -1,14 +1,15 @@
 "use strict";
 const { body, validationResult } = require('express-validator');
+const { check } = require('express-validator');
 
 exports.validateUser = async (req, res, next) => {
     await body('email').isEmail().run(req);
     await body('password').trim().isLength({min:8}).not().isEmpty().run(req);
-
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     } else {
-        next();
-    }
-};
+        next();     
+}};
+
